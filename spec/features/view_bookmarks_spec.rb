4ea7d5bab745dd50a1d905b6254942fdf611822f
Douglas <douglaskerins@gmail.com>
2021-10-12 15:41:02 +0100

    adding test environment database

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -5,7 +5,16 @@ feature "viewing bookmarks" do
   end
 
   scenario "viewing bookmarks" do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
-    expect(page).to have_content(Bookmark.all)
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -1,7 +1,18 @@
 require 'bookmark'
 
+
 describe Bookmark do 
   it '.all returns all bookmark instances' do
-    expect(Bookmark.all).to eq (['http://www.askjeeves.com', 'http://www.twitter.com', 'http://www.google.com'])
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.twitter.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
+    bookmarks = Bookmark.all
+
+    expect(bookmarks).to include('http://www.makersacademy.com')
+    expect(bookmarks).to include('http://www.twitter.com')
+    expect(bookmarks).to include('http://www.google.com')
   end
 end
